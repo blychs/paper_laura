958c474413aae42a90546bf1087b105daedfc040
Your Name <pablo.lichtig@gmail.com>
2022-01-04 15:23:42 -0300

    correccion valores duplicados, max de tmpd y agregado RH

--- meteorologia_arcal_montevideo.py
+++ meteorologia_arcal_montevideo.py
@@ -20,7 +20,7 @@ import pandas as pd
 import xarray as xr
 
 # + tags=[]
-montevideo = pd.read_csv('ARCAL_ISH/montevideo2.csv', delimiter=',')
+montevideo = pd.read_csv('ARCAL_ISH/montevideo2.csv', delimiter=',') # montevideo2 es abrir y volver a cerrar en excel para que ponga bien los delimiters, si no mezcla ; y ,
 montevideo = montevideo[(montevideo['date[yyyymmddHHMM]'] >= 201904031200)]
 fechas = pd.read_csv('fechas.csv')
 fechas['montevideo'] = pd.to_datetime(fechas['montevideo'], format='%d/%m/%Y')
@@ -33,6 +33,19 @@ montevideo['press[hPa]'] = montevideo['press[hPa]'].where(montevideo['press[hPa]
 montevideo['prcp[mm]'] = montevideo['prcp[mm]'].where(montevideo['prcp[mm]'] < 999)
 montevideo['sky[octas]'] = montevideo['sky[octas]'].where(montevideo['sky[octas]'] > 99)
 montevideo['skyOpaque[octas]'] = montevideo['skyOpaque[octas]'].where(montevideo['skyOpaque[octas]'] < 99)
+montevideo['tmpd[C]'] = montevideo['tmpd[C]'].where(montevideo['tmpd[C]'] < 99)
+
+# Calculo RH como rh=100*(EXP((17.625*d)/(243.04+d))/EXP((17.625*t)/(243.04+t)));
+# donde d=dewpoint (dptp) y t=dry temp (tmpd), aprox de August-Roche-Magnus
+d = montevideo['dptp[C]']
+t = montevideo['tmpd[C]']
+montevideo['RH[%]'] = 100 * (np.exp((17.625 * d)/(243.04 + d)) / np.exp((17.625 * t)/(243.04 + t)))
+# 
+
+#Promedio fechas que están dobles
+montevideo = montevideo.groupby('date[yyyymmddHHMM]').mean().reset_index()
+
+
 
 # Parsing de fecha
 datetime = pd.to_datetime(montevideo['date[yyyymmddHHMM]'], format='%Y%m%d%H%M')
@@ -43,8 +56,13 @@ cols = cols[-1:] + cols[:-1] # Mando la última columna (date) al principio
 montevideo = montevideo[cols]
 display(montevideo)
 montevideo = montevideo.set_index('date')
+montevideo['RH[%]'].plot()
 
 display(montevideo)
+# -
+
+plt.ylim([0,150])
+montevideo['tmpd[C]'].plot(style='.')
 
 # + tags=[]
 montevideo = montevideo.groupby(level=0).sum()
@@ -56,15 +74,16 @@ display(montevideo24hs_mean)
 montevideo24hs_sum = montevideo.resample('24H', offset='12h').sum() # Resampleo para tener promedios diarios
 
 montevideo_comb = pd.concat([montevideo24hs_mean[['wdir', 'wspd[m/s]', 'clht[km]', 'hzvs[km]', 'tmpd[C]',
-                                          'slvp[hPa]', 'press[hPa]', 'sky[octas]', 'skyOpaque[octas]']],
+                                          'slvp[hPa]', 'press[hPa]', 'sky[octas]', 'skyOpaque[octas]', 'RH[%]']],
                          montevideo24hs_sum[['prcp[mm]', 'prcpPeriod[hours]']]], axis=1)
 
 display(montevideo_comb)
 montevideo_comb.index = montevideo_comb.index.normalize()
 montevideo_comb = (montevideo_comb.reindex(index = fechas['montevideo'].to_list()))
-montevideo_comb.to_excel('montevideo_meteo.xlsx')
+montevideo_comb.to_excel('montevideo_meteo2.xlsx')
 
+#with pd.
 display(fechas['montevideo'])
 # -
-
+montevideo_comb['RH[%]'].plot(style='.')
 
